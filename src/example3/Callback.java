1db73060a93bf41f7695f7bef7bfb3cdc4562024
Boriz81 <blitzboriz@gmail.com>
2023-02-06 20:09:57 +0900

    Initial commit

---
+++ src/example3/Callback.java
@@ -0,0 +1,13 @@
+package example3;
+
+interface Callback {
+    void calculated(int result);
+
+    void failed(String errorMsg);
+
+
+    //default void print(String result) {
+    //    System.out.println(result);
+    //}
+
+}
